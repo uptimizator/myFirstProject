139e7b5b01c27a07a6c3be5b0b6818a6b775d36a
uptimizator <57381810+uptimizator@users.noreply.github.com>
2020-02-16 00:50:35 -0500

    refactoring and page object

--- spec.js
+++ spec.js
@@ -1,56 +1,63 @@
 //https://docs.google.com/forms/d/e/1FAIpQLSeY_W-zSf2_JxR4drhbgIxdEwdbUbE4wXhxHZLgxZGiMcNl7g/viewform
+const Pg = require('./page').default;
+const futureDate = Cypress.moment().add(5, "days");         //date now + 5 days
+const currentMonth = futureDate.format('MMMM');             //fetch month from the date
+const successText = 'Your response has been recorded.';     //success message
 
-const currentMonth = Cypress.moment().format('MMMM');
-
-describe("", function() {
-    it("First page", function() {
-        cy.visit('https://docs.google.com/forms/d/e/1FAIpQLSeY_W-zSf2_JxR4drhbgIxdEwdbUbE4wXhxHZLgxZGiMcNl7g/viewform')
+describe("Google form test", function() {
+    it("", function() {
+        cy.visit('https://docs.google.com/forms/d/e/1FAIpQLSeY_W-zSf2_JxR4drhbgIxdEwdbUbE4wXhxHZLgxZGiMcNl7g/viewform');
+        //use within method to avoid any wrong selections
         cy.get('form').within(() => {
-            cy.get('[data-value="Check this"]').first().click();
-            cy.get('[data-value="Check this"]').last().click();
-
-            //let futureDateUnix = Cypress.moment().unix() + 6*24*60*60; //date 6 days from now UNIX timestamp
-            //const futureDate = Cypress.moment().unix(futureDateUnix).format("YYYY-MM-DD");   //format unix - to acceptable date
-            let futureDate = Cypress.moment().add(5, "days").format("YYYY-MM-DD");
-            cy.get('input[type="date"]').type(futureDate);
-
-            cy.get('.quantumWizTextinputPaperinputInput').last().as('thirdInput').should('have.attr', 'required');
-            cy.get('@thirdInput').type(currentMonth);
-            //cy.root().submit();
-            cy.get('.appsMaterialWizButtonPaperbuttonLabel').should('have.text', 'Next').click();
+            //click to checkboxes with attributes "check this"
+            Pg.checkThisBoxes;
+            //input date which is N days from now
+            Pg.dateInput.type(futureDate.format("YYYY-MM-DD"));
+            //check input for required parameter
+            Pg.monthInput.should('have.attr', 'required');
+            //type in to input current month
+            Pg.monthInput.type(currentMonth);
+            //go to next page
+            Pg.nextButton.first().should('have.text', 'Next').click();
         });
         
-        //1st input
+        //array of 5 favorit movies
         let myFavMovies = ['home alone', 'spider man', 'friends', 'avengers', 'superman'];
+        //randomly pick 3 movies
         let myFavMoviesThreeRand = myFavMovies.sort(() => Math.random() - Math.random()).slice(0, 3).toString();
-        cy.get('textarea').type(myFavMoviesThreeRand);
+        //type movies in to input
+        Pg.moviesInput.type(myFavMoviesThreeRand);
         //dropdown
-        cy.get('div.quantumWizMenuPaperselectEl[role=listbox]').click().type('{downarrow}{enter}');
-        cy.wait(1000);
-        //go back to 1st page
-        cy.get('.appsMaterialWizButtonPaperbuttonLabel').first().should('have.text', 'Back').click({force: true});
-        
-        //1st page 3d input. Reverse month
+        Pg.colorDropdown.click().type('{downarrow}{enter}');
+        //doesn't save selected item without wait. Probably its to fast. Wait for 0.1sec
+        cy.wait(100);
+        //return to 1st page
+        Pg.backButton.click({force: true});
+        //function to reverse month
         function reverseMonth(currentMonth) {
-            let currentMonthReversed = ""
+            let currentMonthReversed = "";
             for (let i = currentMonth.length - 1; i >= 0; i--) {
                 currentMonthReversed += currentMonth[i];
-            }
+            };
             return currentMonthReversed;
-        }
+        };
+        //store reversed month in to variable
         let currentMonthReversed = reverseMonth(currentMonth);
-        cy.get('input.quantumWizTextinputPaperinputInput').last().as('thirdInput').clear();
-        cy.get('@thirdInput').type(currentMonthReversed);
+        //type reversed month in to input
+        Pg.monthInput.clear().type(currentMonthReversed);
         //return to 2nd page
-        cy.get('.appsMaterialWizButtonPaperbuttonLabel').should('have.text', 'Next').click();
+        Pg.nextButton.should('have.text', 'Next').click();
         //Check that both questions are still filed
-        cy.get('textarea').should('have.attr', 'data-initial-value', myFavMoviesThreeRand);
-        cy.get('div.quantumWizMenuPaperselectOption.isSelected').should('have.attr', 'data-value', 'Red');
+        Pg.moviesInput.should('have.attr', 'data-initial-value', myFavMoviesThreeRand);
+        //assert that input still has value and its equal to 'Red'
+        Pg.colorDropdownSelected.should('have.attr', 'data-value', 'Red');
         //go to last page
-        cy.get('.appsMaterialWizButtonPaperbuttonLabel').last().should('have.text', 'Next').click();
+        Pg.nextButton.last().should('have.text', 'Next').click();
         //check radioButton Yes
-        cy.get('.docssharedWizToggleLabeledLabelText').first().should('have.text', 'Yes').click();
+        Pg.radioButtons.first().should('have.text', 'Yes').click();
         //submit form
-        cy.get('.appsMaterialWizButtonPaperbuttonLabel').last().should('have.text', 'Submit').click();
+        Pg.submitButton.click();
+        //look for succes message
+        cy.contains(successText).should('be.visible');
     });
 });
